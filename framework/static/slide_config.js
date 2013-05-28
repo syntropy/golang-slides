8a2b4e51a85d53e10b0d3c587f705210073c6832
Alexander Surma <surma@surmair.de>
2013-05-28 18:24:14 +0200

    Few initial things working

---
+++ framework/static/slide_config.js
@@ -0,0 +1,32 @@
+var SLIDE_CONFIG = {
+  // Slide settings
+  settings: {
+    useBuilds: true, // Default: true. False will turn off slide animation builds.
+    usePrettify: true, // Default: true
+    enableSlideAreas: true, // Default: true. False turns off the click areas on either slide of the slides.
+    enableTouch: true, // Default: true. If touch support should enabled. Note: the device must support touch.
+    favIcon: 'images/google_developers_logo_tiny.png',
+    fonts: [
+      'Open Sans:regular,semibold,italic,italicsemibold',
+      'Source Code Pro'
+    ],
+  },
+
+  // // Author information
+  // presenters: [{
+  //   name: 'Firstname Lastname',
+  //   company: 'Job Title, Google',
+  //   gplus: 'http://plus.google.com/1234567890',
+  //   twitter: '@yourhandle',
+  //   www: 'http://www.you.com',
+  //   github: 'http://github.com/you'
+  // }/*, {
+  //   name: 'Second Name',
+  //   company: 'Job Title, Google',
+  //   gplus: 'http://plus.google.com/1234567890',
+  //   twitter: '@yourhandle',
+  //   www: 'http://www.you.com',
+  //   github: 'http://github.com/you'
+  // }*/]
+};
+
